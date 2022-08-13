803b5211fe81df013dd99f61803f9ef5739d6c7a
anetastankovska <anetastankovskaane@gmail.com>
2022-08-13 09:55:58 +0200

    Class02

---
+++ Classes/Class02/SEDC.WEBApi.Class02/SEDC.WEBApi.Class02.App/Controllers/FirstController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SEDC.WEBApi.Class02.App.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class FirstController : ControllerBase
+    {
+        private List<string> _names = new List<string>
+        {
+            "Trajan",
+            "Vlatko",
+            "Aneta",
+            "Stefan"
+        };
+
+        [HttpGet]
+        public int GetRandomInteger()
+        {
+            return 1;
+        }
+
+        [HttpGet("name/{id}")]
+        public ActionResult GetName(int id)
+        {
+            try
+            {
+                var name = _names[id];
+                return Ok(name);
+            }
+            catch (Exception)
+            {
+                return NotFound();
+                throw;
+            };
+        }
+    }
+}
