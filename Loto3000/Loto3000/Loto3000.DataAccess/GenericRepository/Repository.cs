e437cb76a7ede169cae43b3f0eee3307ff95b1cb
anetastankovska <anetastankovskaane@gmail.com>
2022-08-30 12:06:23 +0200

    Loto3000 - created solution structure

---
+++ Loto3000/Loto3000/Loto3000.DataAccess/GenericRepository/Repository.cs
@@ -0,0 +1,13 @@
+﻿using Loto3000.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loto3000.DataAccess.GenericRepository
+{
+    public class Repository<T> : IRepository<T> where T : BaseEntity
+    {
+    }
+}
---
+++ Loto3000/Loto3000/Loto3000.DataAccess/IRepository.cs
@@ -0,0 +1,8 @@
+﻿using Loto3000.DataModels;
+
+namespace Loto3000.DataAccess
+{
+    public interface IRepository<T> where T : BaseEntity
+    {
+    }
+}
